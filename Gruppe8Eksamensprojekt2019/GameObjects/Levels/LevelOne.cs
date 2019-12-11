b18f079d52cdb9f1f49453c03d7d25a15ebdb023
thecatmiaw <58418706+thecatmiaw@users.noreply.github.com>
2019-12-11 09:30:56 +0100

    Lappede diverse huller

--- Gruppe8Eksamensprojekt2019/GameObjects/Levels/LevelOne.cs
+++ Gruppe8Eksamensprojekt2019/GameObjects/Levels/LevelOne.cs
@@ -29,17 +29,25 @@ namespace Gruppe8Eksamensprojekt2019
         {
            
 
-            GameObject newSunRay = new SunRay(new Vector2(19 * 96, 13 * 96));
+            GameObject newSunRay = new SunRay(new Vector2(19 * 96* GameWorld.Scale, 13 * 96* GameWorld.Scale));
             GameWorld.collisionObjects.Add(newSunRay);
             GameWorld.gameObjects.Add(newSunRay);
 
-            GameObject newSunRay1 = new SunRay(new Vector2(31 * 96, 12 * 96));
+            GameObject newSunRay1 = new SunRay(new Vector2(31 * 96 * GameWorld.Scale, 12 * 96* GameWorld.Scale));
             GameWorld.collisionObjects.Add(newSunRay1);
             GameWorld.gameObjects.Add(newSunRay1);
 
-            GameObject newSunRay2 = new SunRay(new Vector2(39 * 96, 14 * 96));
+            GameObject newSunRay2 = new SunRay(new Vector2(39 * 96* GameWorld.Scale, 14 * 96* GameWorld.Scale));
             GameWorld.collisionObjects.Add(newSunRay2);
             GameWorld.gameObjects.Add(newSunRay2);
+
+            GameObject newSunRay3 = new SunRay(new Vector2(23 * 96* GameWorld.Scale, 14 * 96* GameWorld.Scale));
+            GameWorld.collisionObjects.Add(newSunRay3);
+            GameWorld.gameObjects.Add(newSunRay3);
+
+            GameObject newSunRay4 = new SunRay(new Vector2(30 * 96* GameWorld.Scale, 17 * 96* GameWorld.Scale));
+            GameWorld.collisionObjects.Add(newSunRay4);
+            GameWorld.gameObjects.Add(newSunRay4);
 
             GenerateLevel(new int[,]
             {
@@ -58,10 +66,10 @@ namespace Gruppe8Eksamensprojekt2019
                 {0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,1,0,4,4,9,9,4,4,0,6,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,4,0,2,4,4,2,0,4,4,0,0,4,2,1,9,1,1,0,0,4,4,0,0,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,2,0,0,4,4,0,0,4,4,0,0,4,4,0,0,0,0,0,0,4,4,0,0,1},
-                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,0,0,0,0,4,8,0,0,1},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,4,4,0,0,4,2,0,0,4,4,0,0,4,4,0,0,0,0,0,0,4,8,0,0,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,4,4,0,0,4,4,0,0,4,4,0,0,4,4,1,0,0,0,0,0,4,4,0,0,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,1,0,9,9,0,0,4,4,0,0,1},
-                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,4,0,0,4,4,8/**/,0,4,4,0,0,4,4,1,0,9,9,0,0,1,1,0,0,1},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,4,0,0,4,4,0,0,4,4,0,0,2,4,1,0,9,9,0,0,1,1,0,0,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,1,0,0,0,0,0,0,0,8,0,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,1,0,0,0,0,0,9,9,0,0,1},
                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1},
